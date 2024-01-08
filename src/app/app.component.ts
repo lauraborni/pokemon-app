553db8a00d613bacce994e9b17e6ef5cebb754ea
Laura Borni <laura.borni@helfrich.fr>
2024-01-08 16:36:57 +0100

    premier commit - affichage liste + eventlistener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from "./mock-pokemons";
 
 @Component({
-  selector: 'app-root',
+  selector: 'pokemon-app',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokemon-app';
+  pokemons: Pokemon[] = null;
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+  }
+  selectPokemon(pokemon: Pokemon) {
+    console.log('Vous avez sélectionné ' + pokemon.name);
+  }
 }
